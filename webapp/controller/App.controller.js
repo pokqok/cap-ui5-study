c737da72a18b5558c3ac87d3bf9c1402b5cbd3ba
pokqok <qamac323@gmail.com>
2026-05-29 10:03:22 +0900

    Step 15: Nested Views

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,15 +1,6 @@
-sap.ui.define(["sap/ui/core/mvc/Controller", "sap/m/MessageToast"], function (Controller, MessageToast) {
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
   "use strict";
 
-  const AppController = Controller.extend("ui5.walkthrough.controller.App", {
-    onShowHello() {
-      // read msg from i18n model
-      const recipient = this.getView()?.getModel()?.getProperty("/recipient/name");
-      const resourceBundle = this.getView()?.getModel("i18n")?.getResourceBundle();
-      const msg = resourceBundle.getText("helloMsg", [recipient]);
-      // show message
-      MessageToast.show(msg);
-    }
-  });
-  return AppController;
+  const App = Controller.extend("ui5.walkthrough.controller.App", {});
+  return App;
 });
